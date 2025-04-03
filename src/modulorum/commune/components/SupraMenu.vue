971d6387f834e66b13ecf68282f4576c5f471c41
Esbosan <esthersanchobosch@gmail.com>
2025-04-03 18:05:10 +0200

    Componentes de daysi ui

--- src/modulorum/commune/components/SupraMenu.vue
+++ src/modulorum/commune/components/SupraMenu.vue
@@ -1,9 +1,9 @@
 <template>
 
 <div class="flex item-center justify-between p-4 bg-base-200">
-
+  <h2 class="text-lg font-bold text-blue-600">Notion App</h2>
 </div>
 
-<h2 class="text-lg font-bold  text-blue-600">Notion App</h2>
+
 
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,23 @@
-import ProjectsLayout from '@/modulorum/projects/layouts/ProjectsLayout.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import ProjectsLayout from '@/modulorum/projects/layouts/ProjectsLayout.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-{
-  path: '/',
-  name: 'projects',
-  component: ProjectsLayout
-}
+    {
+      path: '/',
+      name: 'home',
+      redirect: {name: 'projects'},
+      component: ProjectsLayout,
+      children: [
+        {
+          path: 'projects',
+          name: 'projects',
+          component: () => import('@/modulorum/projects/views/ProjectsView.vue'),
+        },
+      ],
+    },
   ],
-})
+});
 
-export default router
+export default router;
